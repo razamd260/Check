409cc7db3dfba1563333674c8efe5332dac91290
razamd260 <razamd260@gmail.com>
2024-01-29 17:42:09 +0530

    5:59

--- src/test/java/TeatRunner/Test1.java
+++ src/test/java/TeatRunner/Test1.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class Test1 {
      @Test
 	public void test() {
-		System.out.println("Hellow World 1 main");
+		System.out.println("Hellow World 1 main Maan ja Bhai");
 	}
 }
