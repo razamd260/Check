c83aef2ff17093e54d4ce2a77ecd4ddcd1d77fec
razamd260 <razamd260@gmail.com>
2024-01-29 17:45:38 +0530

    Fail

--- src/test/java/TeatRunner/Test1.java
+++ src/test/java/TeatRunner/Test1.java
@@ -1,10 +1,14 @@
 package TeatRunner;
 
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class Test1 {
      @Test
 	public void test() {
 		System.out.println("Hellow World 1 main Maan ja Bhai");
-	}
+		String s="ali";
+		Assert.assertEquals("stsg", s);
+		
+     }
 }
